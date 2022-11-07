eb9bc56b4bfe830633d5aa263e2f18f0a1fdaf0d
BartekFranczak <91423411+BartekFranczak@users.noreply.github.com>
2022-11-07 21:22:55 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,4 +5,38 @@
 class ResourceManager
 {
     // Twoja implementacja tutaj
+        Resource* resource;
+
+    public:
+    ResourceManager()       
+    {   
+        std::cout << "KONSTRUKTOR"<<std::endl;
+        resource = new Resource{};
+    }
+    ~ResourceManager() 
+    {    
+        std::cout << "DESTRUKTOR"<<std::endl;
+        delete resource;
+    }
+
+
+    ResourceManager(const ResourceManager& resource1)   
+    {
+        std::cout << "KONSTRUKTOR KOPIUJACY"<<std::endl;
+        resource = new Resource{};
+        *resource = *resource1.resource;
+    }
+
+    ResourceManager& operator=(const ResourceManager& resource2)
+    {
+        std::cout << "OPERATOR KOPIOWANIA"<<std::endl;    
+        delete this->resource;
+        resource = new Resource{};
+        *resource = *resource2.resource;
+        return *this;
+    }
+    double get() 
+    { 
+        return resource->get(); 
+    }
 };
